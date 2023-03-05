4cefee53776079835505d7956318ff45f80773a8
Santiago Cardoso <104155579+santiagocardoso@users.noreply.github.com>
2023-03-05 14:04:29 -0300

    Add files via upload

---
+++ cp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while (t--) {
+
+	}
+
+	return 0;
+}
+
