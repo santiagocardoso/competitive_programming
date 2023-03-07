d903575d2fbc9e0dbf2f5ccaaf727d9a95f2c03c
Santiago Cardoso <104155579+santiagocardoso@users.noreply.github.com>
2023-03-07 19:08:33 -0300

    Create watermelon.c

---
+++ codeforces/watermelon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int w;
+    scanf("%d", &w);
+    if (w == 2)
+        printf("NO\n");
+    else if (w % 2 == 0)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
+ 
