8438841ae2dff1e1363c8bfd16983338ef162503
Santiago Cardoso <104155579+santiagocardoso@users.noreply.github.com>
2023-03-07 18:50:42 -0300

    Create a.cpp

---
+++ codeforces/817_d4/a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        string name; cin >> name;
+
+        if (n != 5)
+            cout << "NO" << endl;
+        else {
+            int t = 0, i = 0, m = 0, u = 0, r = 0;
+            for (int j = 0; j < n; j++) {
+                if (name[j] == 'T')
+                    t++;
+                if (name[j] == 'i')
+                    i++;
+                if (name[j] == 'm')
+                    m++;
+                if (name[j] == 'u')
+                    u++;
+                if (name[j] == 'r')
+                    r++;
+            }
+            if (t == 1 && i == 1 && m == 1 && u == 1 && r == 1)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
+	}
+
+	return 0;
+}
+ 
